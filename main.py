b9634c8e4577f18ed140ec9f3847c47183280da0
METALNAVY9666 <metalnavy9666@gmail.com>
2022-11-26 22:39:48 +0100

    modification du niveau neo_tokyo

--- main.py
+++ main.py
@@ -2,26 +2,33 @@
 import pygame
 from data.levels import neo_tokyo
 
-pygame.mixer.init()
-
 pack_pygame = {
-    "FPS" : 60,
-    "display" : pygame.display,
-    "mixer" : pygame.mixer,
-    "clock" : pygame.time.Clock()
+    "pygame": pygame,
+    "FPS": 60,
+    "display": pygame.display,
+    "surface": pygame.display.set_mode([1280, 720]),
+    "mixer": pygame.mixer,
+    "clock": pygame.time.Clock()
 }
 
 pygame.init()
+pygame.mixer.init()
 
 FPS = pack_pygame["FPS"]
-
-map = neo_tokyo.NeoTokyo(pack_pygame)
-
 WIN = True
 
-start_time = 0
+current_map = neo_tokyo.NeoTokyo(pack_pygame)
+
 while WIN:
+    # dt est le temps qui s'écoule entre chaque image,
+    # important pour que le jeu reste fluide
     dt = pack_pygame["clock"].tick(FPS)
-    #dt est le temps qui s'écoule entre chaque image
+    # vérifie les évènements
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            WIN = False
+    # actualise la map, si elle renvoie "quit", alors quitter
+    if current_map.update() == "quit":
+        WIN = False
 
 pygame.quit()
