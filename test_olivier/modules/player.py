8c302b8f985d0ac33724c425abee499f5a933f35
Holy-Viet <oliviervo.pro@gmail.com>
2022-12-01 10:29:41 +0100

    Coordonées généralisée dans la classe player.

--- test_olivier/modules/player.py
+++ test_olivier/modules/player.py
@@ -1,7 +1,6 @@
 '''Ce module permet de gérer le joueur, ses déplacements etc'''
 import pygame as pg
 from modules.texture_loader import persos
-from modules.texture_loader import coordinates_list
 
 
 class Player(pg.sprite.Sprite):
@@ -14,7 +13,9 @@ class Player(pg.sprite.Sprite):
         self.rect = self.sprite.get_rect()
         self.rect.x, self.rect.y = 800, 500
         self.sprite_x = 50
-        self.propertie = self.properties()
+        self.tab = []
+        self.coord = self.coordinates_list()
+        self.propertie = self.coord[0]
         # temps entre chaque sprite de l'animation idle en ms
         self.idle_speed = 300
         # somme des temps entres chaque frame
@@ -35,14 +36,7 @@ class Player(pg.sprite.Sprite):
     def move_left(self):
         '''Cette fonction gère les déplacements à gauche.'''
         if self.rect.x > 0:
-            if self.game.name == 'vegeta':
-                self.propertie[1] = 1970
-                self.propertie[2] = 120
-            else:
-                self.propertie[1] = 1990
-                self.propertie[2] = 140
-            self.propertie[0] = 1
-            self.propertie[3] = 105
+            self.propertie = self.coord[1]
             self.rect.x -= 20
 
     def blit_sprite(self, screen, dlt):
@@ -60,3 +54,14 @@ class Player(pg.sprite.Sprite):
             self.delta_sum = 0
         if self.sprite_x > 2:
             self.sprite_x = 0
+
+    def coordinates_list(self):
+        '''Coordonées du spritesheet'''
+        self.tab = [[1, 2, 3, 4], [1, 3, 4, 5]]
+        if self.game.name == 'goku':
+            self.tab = [[111, 890, 113, 120],
+                        [1, 1990, 140, 105]]
+        elif self.game.name == 'vegeta':
+            self.tab = [[200, 1210, 100, 120],
+                        [1, 1970, 120, 105]]
+        return self.tab
