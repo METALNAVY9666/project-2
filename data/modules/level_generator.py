c329ab7a22e37991f8d0ea1dfde0452252bf57d6
METALnavY9666 <metalnavy9666@gmail.com>
2023-01-03 12:33:40 +0100

    restauration d'une ancienne sauvegarde du dossier data, pas d'écran qui zoome mais il bouge c'est déjà pas mal

--- data/modules/level_generator.py
+++ data/modules/level_generator.py
@@ -6,12 +6,6 @@ from tkinter.filedialog import askopenfilename, askdirectory
 buttons = {}
 labels = {}
 
-texts = {
-    "edit": "Modifier le niveau séléctionné",
-    "add": "Ajouter un niveau",
-    "add_bg": "Ajouter un fond"
-}
-
 def read_levels():
     """lis les niveaux"""
     with open("../gfx/levels/levels.json", "r", encoding="utf-8") as file:
@@ -19,37 +13,20 @@ def read_levels():
         file.close()
     return data
 
-def add_bg():
-    """ajoute un niveau"""
-    
-
 def edit_level():
     """modifie le niveau"""
-    ...
 
 root = Tk()
-root.title("Générateur de niveaux")
-root.geometry("480x360")
-root.resizable(False, False)
 
 labels["edit"] = Label(root, text="Modifier un niveau")
 labels["edit"].grid(column=0, row=0)
 
-buttons["edit"] = Button(root, text=texts["edit"], command=edit_level)
-buttons["edit"].grid(column=0, row=1)
-
 levels_box = Listbox(root)
 for level in list(read_levels().keys()):
     levels_box.insert("end", level)
-levels_box.grid(column=0, row=2)
+levels_box.grid(column=0, row=1)
 
-labels["add"] = Label(root, text=texts["add"])
-labels["add"].grid(column=1, row=0, columnspan=2)
-
-buttons["add_bg"] = Button(root, text=texts["add_bg"], command=add_level)
-buttons["add_bg"].grid(column=1, row=1)
-
-labels["bg"] = Label(root)
-labels["bg"].grid(column=1, row=2)
+buttons["edit"] = Button(root, text="Modifier le niveau séléctionné", command=edit_level)
+buttons["edit"].grid(column=0, row=2)
 
 root.mainloop()
--- data/modules/texture_loader.py
+++ data/modules/texture_loader.py
@@ -39,8 +39,9 @@ GFX["exit"] = load_image(UI_PATH+"exit_btn", (X//8, Y//10)).convert()
 
 # chargement des niveaux
 for level in list(levels.keys()):
+    GFX[level] = {}
     TEMP = LEVELS_PATH + level
-    GFX[level] = load_image(TEMP, levels[level]["scale"]).convert()
+    GFX[level]["bg"] = load_image(TEMP, levels[level]["scale"]).convert()
 
 # textures test
 GFX["players"] = {}
