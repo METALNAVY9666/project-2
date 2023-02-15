116720810903b45f9778805913bc2631912cf31c
Alekks17 <alexi.pruvost@gmail.com>
2023-02-15 12:14:12 +0100

    J'ai créer le choix des maps et réglé quelques bug du choix des personnages.

--- test_alexis/main.py
+++ test_alexis/main.py
@@ -1,5 +1,6 @@
 from menu import main as starting
 from choix_perso import main as choix
+from choix_map import main as choix2 
 
 
 def suivant1():
@@ -10,18 +11,35 @@ def suivant1():
         
         if suiv == "retour":
             manager()
+            
+        if suiv[2] == "suivant":
+            suivant2()
+            
+def suivant2():
+    
+    print("SUIVVVVANNNT")
+    suiv = "feur"
+    while suiv != "quitter":
+        suiv = choix2()
+        
+        if suiv == "retour":
+            manager()
+            
 
 
 def manager():
     suiv = "feur"
     
-    while suiv != "quitter":
+    while suiv != "stop":
 
         suiv = starting()
         
         print(suiv)
         if suiv == "choix_perso":
             suivant1()
+            
+        if suiv == "quitter":
+            quit()
         
 manager()
     
