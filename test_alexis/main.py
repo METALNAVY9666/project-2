616e556735b68324f1980d6cb27fc736bf84ddf8
Alekks17 <alexi.pruvost@gmail.com>
2023-02-26 19:02:11 +0100

    J'ai résolu le problème de return des informations (map, perso1, perso2), la fonction debut du Main fonctionne parfaitement et revoies bien les bonnes informations.

--- test_alexis/main.py
+++ test_alexis/main.py
@@ -9,18 +9,19 @@ def debut():
 
     def suivant1(tabinfo):
         
-        suiv = "feur"
-        while suiv != "quitter":
-            suiv = choix()
-            
-            if suiv == "retour":
-                manager(tabinfo)
+        if tabinfo[0] == "":
+            suiv = "feur"
+            while suiv != "quitter":
+                suiv = choix()
                 
-            if suiv[2] == "suivant":
-                tabinfo[1] = suiv[0]
-                tabinfo[2] = suiv[1]
-                print(tabinfo)
-                suivant2(tabinfo)
+                if suiv == "retour":
+                    manager(tabinfo)
+                    
+                if suiv[2] == "suivant":
+                    tabinfo[1] = suiv[0]
+                    tabinfo[2] = suiv[1]
+                    print(tabinfo)
+                    return suivant2(tabinfo)
                 
     def suivant2(tabinfo):
         
@@ -35,7 +36,7 @@ def debut():
             if suiv[1] == "fini":
                 tabinfo[0] = suiv[0]
                 print(tabinfo)
-                manager(tabinfo)
+                return "ok"
                 
                 
             
@@ -52,15 +53,16 @@ def debut():
                 
                 print(suiv)
                 if suiv == "choix_perso":
-                    suivant1(tabinfo)
+                    return suivant1(tabinfo)
                     
                 if suiv == "quitter":
                     quit()
         
-        print(tabinfo)
-        return tabinfo
-
-    return manager(tabinfo)
+    print(tabinfo)
+    manager(tabinfo)
+    print(tabinfo)
+    return tabinfo
+    
 
 debut()
     
