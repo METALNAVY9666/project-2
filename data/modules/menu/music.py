50f8f89184099eb43ad494c0072faa63f24d612d
METALNAVY9666 <metalnavy9666@gmail.com>
2023-03-01 21:59:45 +0100

    ajout musique menu

---
+++ data/modules/menu/music.py
@@ -0,0 +1,29 @@
+from data.modules.settings import read_settings
+
+class Music:
+    """joue de la musique en fonction du type de musique de manière plus simple"""
+
+    def __init__(self, mixer, music):
+        self.loader = mixer.music
+
+        filepath = self.get_file_path("music", music)
+        self.loader.load(filepath)
+
+        self.init_settings()
+
+    def init_settings(self):
+        """initialise les paramètres"""
+        settings = read_settings()
+        self.loader.set_volume(settings["audio"]["music"] / 100)
+
+    def get_file_path(self, category, music):
+        """renvoie le chemin du fichier"""
+        return f"data/sfx/{category}/{music}.mp3"
+
+    def play(self):
+        """ Joue l'audio """
+        self.loader.play()
+
+    def stop(self):
+        """équivoque hein"""
+        self.loader.stop()
